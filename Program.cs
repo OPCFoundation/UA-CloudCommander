905203d91e8bbb4bcc55838f36547820982746f6
barnstee <erichb@microsoft.com>
2024-10-10 17:45:21 -0700

    Remove dependency on Azure Storage and upgrade to latest NuGets.

--- Program.cs
+++ Program.cs
@@ -11,8 +11,6 @@ namespace Opc.Ua.Cloud.Commander
 
     public class Program
     {
-        private static AzureFileStorage _storage = new AzureFileStorage();
-
         public static async Task Main()
         {
             InitLogging(Directory.GetCurrentDirectory());
@@ -36,8 +34,6 @@ namespace Opc.Ua.Cloud.Commander
             fileContent = fileContent.Replace("UndefinedAppName", appName);
             File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "UA.Cloud.Commander.Config.xml"), fileContent);
 
-            LoadCertsFromCloud(appName);
-
             await app.LoadApplicationConfiguration(false).ConfigureAwait(false);
 
             bool certOK = await app.CheckApplicationInstanceCertificate(false, 0).ConfigureAwait(false);
@@ -45,11 +41,7 @@ namespace Opc.Ua.Cloud.Commander
             {
                 throw new Exception("Application instance certificate invalid!");
             }
-            else
-            {
-                StoreCertsInCloud();
-            }
-
+            
             // create OPC UA cert validator
             app.ApplicationConfiguration.CertificateValidator = new CertificateValidator();
             app.ApplicationConfiguration.CertificateValidator.CertificateValidation += new CertificateValidationEventHandler(OPCUAServerCertificateValidationCallback);
@@ -103,111 +95,6 @@ namespace Opc.Ua.Cloud.Commander
 
             Log.Logger = loggerConfiguration.CreateLogger();
             Log.Logger.Information($"Log file is: {Path.Combine(pathToLogFile, "uacloudcommander.logfile.txt")}");
-        }
-
-        private static void StoreCertsInCloud()
-        {
-            // store app certs
-            foreach (string filePath in Directory.EnumerateFiles(Path.Combine(Directory.GetCurrentDirectory(), "pki", "own", "certs"), "*.der"))
-            {
-                _storage.StoreFileAsync(filePath, File.ReadAllBytesAsync(filePath).GetAwaiter().GetResult()).GetAwaiter().GetResult();
-            }
-
-            // store private keys
-            foreach (string filePath in Directory.EnumerateFiles(Path.Combine(Directory.GetCurrentDirectory(), "pki", "own", "private"), "*.pfx"))
-            {
-                _storage.StoreFileAsync(filePath, File.ReadAllBytesAsync(filePath).GetAwaiter().GetResult()).GetAwaiter().GetResult();
-            }
-
-            // store trusted certs
-            foreach (string filePath in Directory.EnumerateFiles(Path.Combine(Directory.GetCurrentDirectory(), "pki", "trusted", "certs"), "*.der"))
-            {
-                _storage.StoreFileAsync(filePath, File.ReadAllBytesAsync(filePath).GetAwaiter().GetResult()).GetAwaiter().GetResult();
-            }
-        }
-
-        private static void LoadCertsFromCloud(string appName)
-        {
-            try
-            {
-                // load app cert from storage
-                string certFilePath = _storage.FindFileAsync(Path.Combine(Directory.GetCurrentDirectory(), "pki", "own", "certs"), appName).GetAwaiter().GetResult();
-                byte[] certFile = _storage.LoadFileAsync(certFilePath).GetAwaiter().GetResult();
-                if (certFile == null)
-                {
-                    Console.WriteLine("Could not load cert file, creating a new one. This means the new cert needs to be trusted by all OPC UA servers we connect to!");
-                }
-                else
-                {
-                    if (!Path.IsPathRooted(certFilePath))
-                    {
-                        certFilePath = Path.DirectorySeparatorChar.ToString() + certFilePath;
-                    }
-
-                    if (!Directory.Exists(Path.GetDirectoryName(certFilePath)))
-                    {
-                        Directory.CreateDirectory(Path.GetDirectoryName(certFilePath));
-                    }
-
-                    File.WriteAllBytes(certFilePath, certFile);
-                }
-
-                // load app private key from storage
-                string keyFilePath = _storage.FindFileAsync(Path.Combine(Directory.GetCurrentDirectory(), "pki", "own", "private"), appName).GetAwaiter().GetResult();
-                byte[] keyFile = _storage.LoadFileAsync(keyFilePath).GetAwaiter().GetResult();
-                if (keyFile == null)
-                {
-                    Console.WriteLine("Could not load key file, creating a new one. This means the new cert generated from the key needs to be trusted by all OPC UA servers we connect to!");
-                }
-                else
-                {
-                    if (!Path.IsPathRooted(keyFilePath))
-                    {
-                        keyFilePath = Path.DirectorySeparatorChar.ToString() + keyFilePath;
-                    }
-
-                    if (!Directory.Exists(Path.GetDirectoryName(keyFilePath)))
-                    {
-                        Directory.CreateDirectory(Path.GetDirectoryName(keyFilePath));
-                    }
-
-                    File.WriteAllBytes(keyFilePath, keyFile);
-                }
-
-                // load trusted certs
-                string[] trustedcertFilePath = _storage.FindFilesAsync(Path.Combine(Directory.GetCurrentDirectory(), "pki", "trusted", "certs")).GetAwaiter().GetResult();
-                if (trustedcertFilePath != null)
-                {
-                    foreach (string filePath in trustedcertFilePath)
-                    {
-                        byte[] trustedcertFile = _storage.LoadFileAsync(filePath).GetAwaiter().GetResult();
-                        if (trustedcertFile == null)
-                        {
-                            Console.WriteLine("Could not load trusted cert file " + filePath);
-                        }
-                        else
-                        {
-                            string localFilePath = filePath;
-
-                            if (!Path.IsPathRooted(localFilePath))
-                            {
-                                localFilePath = Path.DirectorySeparatorChar.ToString() + localFilePath;
-                            }
-
-                            if (!Directory.Exists(Path.GetDirectoryName(localFilePath)))
-                            {
-                                Directory.CreateDirectory(Path.GetDirectoryName(localFilePath));
-                            }
-
-                            File.WriteAllBytes(localFilePath, trustedcertFile);
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Cloud not load cert or private key files, creating a new ones. This means the new cert needs to be trusted by all OPC UA servers we connect to!: " + ex.Message);
-            }
         }
     }
 }
